3515460472fdd47a12afe00dd6a92f54371da660
kilisoria <cristiansoria@gmail.com>
2021-08-12 22:01:36 -0300

    initial components

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let App;
+</script>
+
+<main>
+	<h1>Welcome to {App.title}!</h1>
+	<p>Here you can find the items most sold on mercadolibre.com</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
